8a76697f6006cb9f7d16e31e68853fc21f383447
shashibaranwal <shashibaranwal09@gmail.com>
2026-02-03 15:28:53 +0545

    add note, resume components

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import './app.scss'
 import Dock from './components/Dock'
 import Nav from './components/Nav'
 import Github from './components/Windows/Github'
+import Note from './components/Windows/Note'
+import Resume from './components/Windows/Resume'
 
 const App = () => {
   return (
     <>
       <main>
         <Nav />
-        <Dock />
         <Github />
+        <Note />
+        <Resume />
+        <Dock />
       </main>
     </>
   )
