82a60669aaa7390ea2f2e8bf9534353254076cb2
shashibaranwal <shashibaranwal09@gmail.com>
2026-02-03 15:24:14 +0545

    update API call

--- src/apis/GithubApi.js
+++ src/apis/GithubApi.js
@@ -9,8 +9,3 @@ export const getGithubRepos = async () => {
     const response = await axios.get("https://api.github.com/users/shashibaranwal/repos");
     return response.data;
 }
-
-export const getGithubProfileImg = async () => {
-    const response = await axios.get("https://api.github.com/users/shashibaranwal");
-    return response.data.avatar_url;
-}
