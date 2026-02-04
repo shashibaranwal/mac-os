c1947ee357ad0d5219c015687fa0787b5725e888
shashibaranwal <shashibaranwal09@gmail.com>
2026-02-04 13:50:33 +0545

    add website loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,39 @@
-import React from 'react'
-import './app.scss'
-import Dock from './components/Dock'
-import Nav from './components/Nav'
-import Github from './components/Windows/Github'
-import Note from './components/Windows/Note'
-import Resume from './components/Windows/Resume'
+import React, { useState, useEffect } from "react";
+import "./app.scss";
+import Dock from "./components/Dock";
+import Nav from "./components/Nav";
+import Github from "./components/Windows/Github";
+import Note from "./components/Windows/Note";
+import Resume from "./components/Windows/Resume";
+import Loader from "./components/Loader";
+import Youtube from "./components/Windows/Youtube";
+import Cli from "./components/Windows/Cli";
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
   return (
     <>
-      <main>
-        <Nav />
-        <Github />
-        <Note />
-        <Resume />
-        <Dock />
-      </main>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <main>
+          <Nav />
+          <Github />
+          <Note />
+          <Resume />
+          <Youtube />
+          <Cli />
+          <Dock />
+        </main>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
